8c3aeb939f6d9daaa62f4d7d86aee45e7d2190df
nenekelvin <karvinmitchar@gmail.com>
2019-09-28 09:07:53 +0300

    code cleanup

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h1>Idea Board</h1>
+          <h1>Ideas Board</h1>
         </div>
         <IdeasContainer />
       </div>
